8dc92f0b3c22580e97a2e95f4776282fcc0e37a4
gulsenaktas <aaktasgulsen@gmail.com>
2021-02-03 17:21:26 +0300

    1.1 currency api added

--- test.js
+++ test.js
@@ -1,16 +1,3 @@
-// function calculateCurrency(fromTarget, toTarget, amountValue) {
-//     if (!fromTarget && !toTarget) return "Seçim yapmalısınız!"
-//     if (!fromTarget) return "Çevrilecek değer seçilmedi";
-//     if (!toTarget) return "Çevrilmek istenen değer seçilmedi.";
-//     if (fromTarget.value === toTarget.value) return "Farklı seçimler yapmalısınız!"
-//     if(isNaN(Number(amountValue))) return "Amount olarak sayı girmelisiniz."
-//     const currentCurrencyObject = data[fromTarget.value];
-//     const resultForOne = currentCurrencyObject[toTarget.value];
-//     const result = amountValue * resultForOne;
-//     return amountValue + " " + fromTarget.value + " = " + result + " " + toTarget.value;
-//   }
-
-
 const data={
     USD: {EUR: 0.82, GBP: 0.74, TRY: 7.63},
     EUR: {USD: 1.23, GBP: 0.91, TRY: 9.34},
@@ -18,7 +5,7 @@ const data={
     TRY: {USD: 0.13, EUR: 0.11, GBP: 0.09},
 }
 
-const currencyKeys = Object.keys(data) 
+const currencyKeys = Object.keys(data)
 
 function createCurrencyElements(element, root, inputName){
     for (let i = 0; i < element.length; i++) {
@@ -39,9 +26,8 @@ function createCurrencyElements(element, root, inputName){
         currencyKeyDiv.append(currencyKeyLabel)
 
         root.append(currencyKeyDiv)
-
-    }
-}
+    };
+};
 
 const parentEl = document.querySelector("#currency-box-from");
 const fromInputName = "currency_from";
@@ -56,15 +42,13 @@ createCurrencyElements(currencyKeys, parentToEl, toInputName);
 const calculateButton = document.querySelector("#calculate-button");
 calculateButton.addEventListener("click", function(){
     
-    const currencyResult = document.querySelector("#currency-result"); 
-    const amount = document.querySelector("input[name='amount']").value       
+    const currencyResult = document.querySelector("#currency-result");
     const fromTarget = document.querySelector("input[name='currency_from']:checked");
     const toTarget = document.querySelector("input[name='currency_to']:checked");
 
-    // currencyResult.innerHTML = calculateCurrency(fromTarget, toTarget, amount);
 
     if(fromTarget==null || toTarget==null){
-        currencyResult.innerHTML = '" choose currency! "'   
+        currencyResult.innerHTML = '" choose currency! "'
 
     }else {
         const amount = document.querySelector("input[name='amount']").value
@@ -73,20 +57,47 @@ calculateButton.addEventListener("click", function(){
         
         const currentCurrencyObject = data[fromTarget.value];
         const resultForOne = currentCurrencyObject[toTarget.value];
-        const result = amount * resultForOne;
+        const result = (amount * resultForOne).toFixed(2);
 
 
         if(fromTarget.value==toTarget.value){
-            currencyResult.innerHTML = '" choose different currency! "' ;  
+            currencyResult.innerHTML = '" choose different currency! "' ;
         }else{
             if(isNaN(Number(amount))){
                 currencyResult.innerHTML = '" amount should be a number! "'
                 inputBorder.style= `border: 1px solid red;`
             }else{
                 currencyResult.innerHTML = amount + " " + fromTarget.value + " = " + result + " " + toTarget.value;
-                inputBorder.style= `border: 3px solid  #3d3180;`    
+                inputBorder.style= `border: 3px solid  #3d3180;`
             }
-        }     
+        }
     }
-})
+});
 
+const getData = async function(currency){
+    const response =  await fetch(`https://api.exchangeratesapi.io/latest?base=${currency}`);
+    const responseJSON = await response.json();
+
+    return responseJSON
+};
+
+getData("USD").then(e=>{
+    data.USD.EUR = e.rates.EUR
+    data.USD.GBP = e.rates.GBP
+    data.USD.TRY = e.rates.TRY
+});
+getData("EUR").then(e=>{
+    data.EUR.USD = e.rates.USD
+    data.EUR.GBP = e.rates.GBP
+    data.EUR.TRY = e.rates.TRY
+});
+getData("GBP").then(e=>{
+    data.GBP.EUR = e.rates.EUR
+    data.GBP.USD = e.rates.USD
+    data.GBP.TRY = e.rates.TRY
+});
+getData("TRY").then(e=>{
+    data.TRY.EUR = e.rates.EUR
+    data.TRY.GBP = e.rates.GBP
+    data.TRY.USD = e.rates.USD
+});
